d2e5509ea227a874f0af7e8f629359e9f09e92b8
xiuyuhang <442367943@qq.com>
2018-03-12 19:36:00 +0800

    first

---
+++ src/main/java/shiwuliang/com/App.java
@@ -0,0 +1,13 @@
+package shiwuliang.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
