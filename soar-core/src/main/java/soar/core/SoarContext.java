1b10c79b9b47ba7271b62a28fbc414543cbfd9f8
xiuyuhang <442367943@qq.com>
2018-03-14 21:16:37 +0800

    sth

--- soar-core/src/main/java/soar/core/SoarContext.java
+++ soar-core/src/main/java/soar/core/SoarContext.java
@@ -7,4 +7,11 @@ package soar.core;
  * @since 2018-03-14
  */
 public class SoarContext {
+
+    private static final ThreadLocal<SoarContext> LOCAL = new ThreadLocal<SoarContext>() {
+        @Override
+        protected SoarContext initialValue() {
+            return new SoarContext();
+        }
+    };
 }
