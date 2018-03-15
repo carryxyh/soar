29b7418b100702835e3b34eef13a6008243ab241
xiuyuhang <442367943@qq.com>
2018-03-15 20:08:52 +0800

    registry

--- soar-registry/src/main/java/soar/registry/Registry.java
+++ soar-registry/src/main/java/soar/registry/Registry.java
@@ -7,4 +7,18 @@ package soar.registry;
  * @since 2018-03-14
  */
 public interface Registry {
+
+    /**
+     * registry a path to center
+     *
+     * @param path the registry path
+     */
+    void registry(String path);
+
+    /**
+     * unregistry a path
+     *
+     * @param path the path
+     */
+    void unRegistry(String path);
 }
---
+++ soar-registry/src/main/java/soar/registry/curator/CuratorRegistry.java
@@ -0,0 +1,49 @@
+package soar.registry.curator;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.log4j.Logger;
+import soar.registry.Registry;
+
+/**
+ * CuratorRegistry
+ *
+ * @author xiuyuhang [xiuyuhang]
+ * @since 2018-03-15
+ */
+public class CuratorRegistry implements Registry {
+
+    private static final Logger logger = Logger.getLogger(CuratorRegistry.class);
+
+    /**
+     * client
+     */
+    private final CuratorFramework client;
+
+    public CuratorRegistry(String server, String digest) {
+        CuratorFrameworkFactory.Builder builder = CuratorFrameworkFactory.builder()
+                .connectString(server)
+                .retryPolicy(new RetryNTimes(1, 1000))
+                .connectionTimeoutMs(5000);
+        try {
+            if (digest != null) {
+                builder.authorization("digest", digest.getBytes("utf-8"));
+            }
+            client = builder.build();
+        } catch (Exception e) {
+            logger.error("create CuratorRegistry fail !", e);
+            throw new IllegalStateException(e.getMessage());
+        }
+    }
+
+    @Override
+    public void registry(String path) {
+
+    }
+
+    @Override
+    public void unRegistry(String path) {
+
+    }
+}
