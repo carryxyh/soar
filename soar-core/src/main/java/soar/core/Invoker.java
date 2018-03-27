df61597c9b853dce2e9759701cf35ca1dad4be61
xiuyuhang <442367943@qq.com>
2018-03-27 20:28:52 +0800

    sth

--- soar-core/src/main/java/soar/core/Invoker.java
+++ soar-core/src/main/java/soar/core/Invoker.java
@@ -4,11 +4,12 @@ import soar.common.SoarException;
 
 /**
  * Invoker
+ * the provider service for consumer
  *
  * @author xiuyuhang [xiuyuhang]
  * @since 2018-03-14
  */
-public interface Invoker<T> {
+public interface Invoker<T> extends Domain {
 
     /**
      * invoke a remote request
