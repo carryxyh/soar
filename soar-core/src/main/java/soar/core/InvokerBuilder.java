e60ccddbaa86c28361ee0417c8c8c319482982d4
xiuyuhang <442367943@qq.com>
2018-04-02 20:07:12 +0800

    invoker builder

--- soar-core/src/main/java/soar/core/InvokerBuilder.java
+++ soar-core/src/main/java/soar/core/InvokerBuilder.java
@@ -1,11 +1,28 @@
 package soar.core;
 
+import soar.common.Invoker;
+import soar.common.InvokerData;
+
+import java.util.List;
+
 /**
  * InvokerBuilder
+ * 1.build a invoker with invoker data
  *
  * @author xiuyuhang [xiuyuhang]
  * @since 2018-03-14
  */
-public class InvokerBuilder {
+public final class InvokerBuilder<T> {
 
+    /**
+     * build a invoker with given data
+     *
+     * @param datas server data
+     * @return SoarInvoker if {@code datas} size is 1
+     * , ClusterInvoker if {@code datas} is all from the same room
+     * , RoomRouterInvoker if {@code datas} is from different rooms
+     */
+    public Invoker<T> build(List<InvokerData> datas) {
+        return null;
+    }
 }
